f0ddf6e6228b3d144998a2385a185848baf52084
Galex <galexu@gmail.com>
2023-06-20 17:51:47 +0200

    primera llamada api

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,27 @@
+<script setup>
+import { ref } from 'vue';
+
+// const nombre = ref('Juan');
+// const nombre = ref([]);
+// nombre.value = 'Luis';
+
+
+const urlApi = 'https://restcountries.com/v3.1/region/europe';
+
+function recibirCountries() {
+  fetch(urlApi)
+  .then((respuesta) => respuesta.json())
+  .then((data) => {
+    console.log(data)
+    countries.value = data;
+  })
+}
+
+recibirCountries();
+
+const countries = ref([]);
+</script>
+
 <template>
-  App Limpia
+  {{ countries }}
 </template>
